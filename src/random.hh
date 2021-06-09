8c442a0b76285b55a079f748f144d9786a922a17
Raphael H Ribas <raphael.ribas@gmail.com>
2021-06-09 15:49:04 -0400

    inline

--- src/random.hh
+++ src/random.hh
@@ -8,8 +8,8 @@ std::uniform_int_distribution<uint32_t> uint32_interval(0, 0xffffffffu);
 std::uniform_int_distribution<uint64_t> uint64_interval(0,
                                                         0xffffffffffffffffllu);
 
-uint32_t rand32() { return uint32_interval(rng); }
+inline uint32_t rand32() { return uint32_interval(rng); }
 
-uint64_t rand64() { return uint64_interval(rng); }
+inline uint64_t rand64() { return uint64_interval(rng); }
 
 #endif
